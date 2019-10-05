ebb05621c0f6fd483ec8a342835592dc04db43b6
Igor Aleksanov <popzxc@yandex.ru>
2019-10-05 11:34:21 +0300

    Update setup script

--- python_runtime/setup.py
+++ python_runtime/setup.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python
-"""Setup script for the Exonum python runtime."""
-from distutils.core import setup
+"""Setup Script for the Exonum Python Runtime."""
+import setuptools
 
 INSTALL_REQUIRES = ["protobuf", "pysodium", "tornado"]
 
-PYTHON_REQUIRES = ">=3.4"
+PYTHON_REQUIRES = ">=3.7"
 
-setup(
+with open("README.md", "r") as readme:
+    LONG_DESCRIPTION = readme.read()
+
+setuptools.setup(
     name="exonum-python-runtime",
-    version="0.1",
+    version="0.1.0",
+    author="The Exonum team",
+    author_email="contact@exonum.com",
     description="Exonum Python Runtime",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
     url="https://github.com/popzxc/exonum-python-backend",
-    packages=["exonum_runtime"],
+    packages=setuptools.find_packages(),
     install_requires=INSTALL_REQUIRES,
     python_requires=PYTHON_REQUIRES,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Security :: Cryptography",
+    ],
 )
